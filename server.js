4f2198142cc64a0b3148b84ce218b8a617f9f4ff
Davlatyar"git config --global user.name Davlatyargit config --global user.name Davlatyar <davlatyar2008@gmail.com>
2025-10-28 17:58:45 +0500

    done

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
+import TelegramBot from "node-telegram-bot-api"
+
 const TOKEN = "8279320933:AAFh8UHkrn6HwKUBWOBg2HWx0WsmZr4OWlA"
-console.log("Hello World");
+
+const bot = new TelegramBot(TOKEN, { polling: true })
+
+bot.on("message", function(msg){
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    bot.sendMessage(chatId, `Salom --> ${text}`);
+    console.log("Xabar keldi");
+    console.log(msg);    
+})
+
+console.log("Botimiz ishga tushdi");
