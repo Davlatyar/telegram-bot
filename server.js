5eda346bea862d9d5960022466f471a5b9a5e29c
davlatyar <davlatyar2008@gmail.com>
2025-10-31 20:28:31 +0500

    bot photos and info

--- server.js
+++ server.js
@@ -4,17 +4,41 @@ const TOKEN = "8279320933:AAFh8UHkrn6HwKUBWOBg2HWx0WsmZr4OWlA"
 
 const bot = new TelegramBot(TOKEN, { polling: true })
 
+bot.setMyCommands([
+    {
+        command: "/start",
+        description: "Bot haqida ma'lumot"
+    },
+    {
+        command: "info",
+        description: "Ozingiz haqida malumot"
+    }
+])
+
+
 bot.on("message", function(msg){
     const chatId = msg.chat.id;
     const text = msg.text;
-
-    // bot.sendMessage(chatId, `Salom --> ${text}`);
+  
+  
     if(text === "/start") {
-        bot.sendMessage(
-            chatId,
-            "Assalomu Aleykum Davlatyorni telegram botiga hush kelibsiz"
+        return bot.sendMessage(
+            chatId, 
+            `Assalomu aleykum ${msg.from?.first_name}`
         )
     }
+
+    if(text === "/info") {
+        bot.sendPhoto(
+            chatId,
+            "https://images.unsplash.com/photo-1497316730643-415fac54a2af?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+        )
+        return bot.sendMessage(
+            chatId,
+            `Sizning telegram username ${msg.from?.username}`
+        )
+    }
+
     console.log("Xabar keldi");
     console.log(msg);    
 })
