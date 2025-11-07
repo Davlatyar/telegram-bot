a1d154cb90ea9e0c122971c40e76fbf1e528a7d8
davlatyar <davlatyar2008@gmail.com>
2025-11-07 23:20:07 +0500

    project

--- server.js
+++ server.js
@@ -5,65 +5,6 @@ const TOKEN = "8279320933:AAFh8UHkrn6HwKUBWOBg2HWx0WsmZr4OWlA"
 const bot = new TelegramBot(TOKEN, { polling: true })
 
 
-const obj = {}
-
-const gameOptions = {
-    reply_markup: {
-        inline_keyboard: [
-            [
-                {
-                    text: "1",
-                    callback_data: "1",
-                },
-                {
-                    text: "2",
-                    callback_data: "2",
-                },
-                {
-                    text: "3",
-                    callback_data: "3",
-                },
-            ],
-            [
-                {
-                    text: "4",
-                    callback_data: "4",
-                },
-                {
-                    text: "5",
-                    callback_data: "5",
-                },
-                {
-                    text: "6",
-                    callback_data: "6",
-                },
-            ],
-            [
-                
-                {
-                    text: "7",
-                    callback_data: "7",
-                },
-                {
-                    text: "8",
-                    callback_data: "8",
-                },
-                {
-                    text: "9",
-                    callback_data: "9",
-                },
-            ],
-            [
-                {
-                    text: "0",
-                    callback_data: "0",
-                },
-                
-            ]
-
-        ]
-    }
-}
 
 const bootstrap = () => {
     bot.setMyCommands([
@@ -75,10 +16,6 @@ const bootstrap = () => {
             command: "info",
             description: "Ozingiz haqida malumot"
         },
-        {
-            command: "/game",
-            description: "O'yin o'ynash"
-        }
     ])
 
 
@@ -106,26 +43,39 @@ const bootstrap = () => {
         } else if (text == "Boshlash 🔥") {
           const xabar = await bot.sendMessage(chatId, "1 sekund..");
 
-          setTimeout(function () {
+          setTimeout(function() {
              bot.deleteMessage(chatId,xabar.message_id)
              bot.sendPhoto(
-                 chatId, "./photos/bmv-m5.jpg"
-                ),
-                {
-                    reply_markup:{
-                        inline_keyboard:[
-                            [{text: "Sotip olish"}],                            
-                        ]
-                    }
-                }
+                 chatId, "./photos/bmv-m5.jpg", {
+                    caption: "Siz BMV M5 sotib olishga rozimisiz? \n\ The F90 M5 is based on the G30 5 Series\n\ nand uses an all-wheel drive powertrain, being\nthe first time that an M5 has not been rear-wheel.",
+                    reply_markup: {
+                        inline_keyboard: [
+                          [
+                            { text: "Rasmlar", callback_data: "photos" },
+                            { text: `Ma'lumot`, callback_data: "info" },
+                          ],
+                          [{ text: "Sotib olish", callback_data: "buy" }],
+                        ],
+                      },
+                    
+                },
+                )
             }, 1000)
-                
-
         }else if (text == "Menu 🍔") {
             bot.sendMessage(chatId, "Menuga hush kelibsiz")
+            bot.sendPhoto(chatId,
+                "photos/menu.jfif"
+            )
+        }else if (text == "Sozlamalar ⚙️") {
+            bot.sendMessage(chatId,
+                "Mana Malumotlar"
+            )
+            bot.sendPhoto(chatId,
+                "./photos/settings.jpg",
+            )
         }
     
-        if(text === "/info") {
+        if(text === "info") {
             bot.sendPhoto(
                 chatId,
                 "https://images.unsplash.com/photo-1497316730643-415fac54a2af?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
@@ -135,39 +85,52 @@ const bootstrap = () => {
                 `Sizning telegram username ${msg.from?.username}`
             )
         }
-        if (text === "/game") {
-           bot.sendMessage(
-                chatId,
-                "Birdan 1 dan 10 gacha sonni toping",
-                gameOptions
-            );
-            const randomNumber =  Math.floor(Math.random() * 10);
-            obj[chatId] = randomNumber;
-            return randomNumber
-            // console.log(randomNumber);
-          }
-          
-
-        //  bot.sendMessage(
-        //     chatId,
-        //     "Uzur men sizning gapingizga tushunmadim"
-        // )
     })
 }
-    bot.on("callback_query", msg => {
-     const data = msg.data;
-     const chatId = msg.message.chat.id;
+    bot.on("callback_query", function(query){
+        console.log(query);
+        
+        const chatId = query.message.chat.id;
+        const data = query.data;
+   
 
-     if(data == obj[chatId]) {
-         return bot.sendMessage(chatId,
-            `Tabriklaymiz siz togri toptingiz, javob ${obj[chatId]} edi `)
-            
-        }else{
-            return bot.sendMessage(
-                chatId, `siz ${data}ni tanladinggiz bu hato, togri javod ${obj[chatId]} edi`
+        if (data == "buy") {
+            bot.sendMessage(
+                chatId, "Siz 120 000$ tolashga rozimisiz?",
+                {
+                    reply_markup: {
+                        inline_keyboard:[
+                            [{text: "Ha",callback_data:"confrm"}],
+                            [{text: "Yoq",callback_data:"reject"}]
+                        ],
+                    },
+                },
             )
-     }
-})
+        }else if (data == "confrm") {
+            bot.sendMessage(chatId, "Tabriklaymiz endi siz BMV M5 F90 li boldingiz!!")      
+        }else if (data == "reject") {
+            bot.sendMessage(chatId, "Bekor qilindi")
+        }
+        
+        if (data == "photos") {
+            bot.sendMessage(chatId, "BMV M5ning RASMLARI")
+            bot.sendPhoto(chatId, "./photos/bm5-photo1.jfif")
+            bot.sendPhoto(chatId, "./photos/bm5-photo2.jfif")
+        }
+
+        if(data == "info") {
+            bot.sendMessage(
+                chatId,
+                "BMV M5 HAQIDA MALUMOT, \n\ The BMW M5 F90 is the sixth generation of the high-performance sedan produced from 2017 to 2023. \n\ A notable departure from its predecessors, it was the first M5 to come standard with a rear-biased all-wheel-drive system, called M xDrive."
+            )
+        }
+        if(data == "info") {
+            bot.sendPhoto(chatId,
+                "./photos/settings.jpg"
+            )
+        }
+    }
+)
 
 bootstrap()
 console.log("Botimiz ishga tushdi");
